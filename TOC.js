33fdba8e4a25f6adefbf6e76cfcf0a6e7608dc65
trendiinfo <34641547+trendiinfo@users.noreply.github.com>
2018-11-05 22:13:06 +0700

    Add files via upload

---
+++ TOC.js
@@ -0,0 +1,6 @@
+ <div id="btn_toc" onclick="if (document.getElementById('toc').style.display === 'none') { document.getElementById('toc').style.display = 'block'; } else { document.getElementById('toc').style.display = 'none'; }" role="button" tabindex="0">Contents <svg width="18" height="18" viewBox="0 0 24 24"><path fill="#000000" d="M12,18.17L8.83,15L7.42,16.41L12,21L16.59,16.41L15.17,15M12,5.83L15.17,9L16.58,7.59L12,3L7.41,7.59L8.83,9L12,5.83Z" /></svg></div>
+<div id="toc">
+<ol>
+  <li><a href="#toc_1" title="Download">Download" /></a></li>
+  </ol>
+</div>
